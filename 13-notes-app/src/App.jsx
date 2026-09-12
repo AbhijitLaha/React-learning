b0fb655f4be94705a0c73fc7aff78a4ada0c9d51
Abhijit Laha <lahaabhi7@gmail.com>
2026-09-12 23:57:44 +0530

    feat: enhance form structure and styling for notes input

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+
+  const submitHandler = (e) =>{
+    e.preventDefault()
+
+  }
+
   return (
-    <div>
-      <form className='flex justify-between'>
-        <input 
-        type="text" 
-        placeholder='Enter Notes Heading' 
-        />
-        <input type="text" 
-        className='' 
-        placeholder='Write details' 
-        />
+    <div className="h-screen bg-black text-white">
+      <form onSubmit={(e) =>{
+        submitHandler(e)
+      }} className="flex justify-between gap-5 flex-col items-start p-10">    
+          <input
+            type="text"
+            placeholder="Enter Notes Heading"
+            className="px-5 py-2 font-medium w-full border-2 rounded"
+          />
+          <textarea
+            type="text"
+            className="px-5 py-2 h-40 font-medium flex items-start flex-row w-full border-2 rounded"
+            placeholder="Write details"
+          />
+          <button className="px-5 py-2 w-full font-medium bg-white text-black  rounded">
+            Add Note
+          </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
